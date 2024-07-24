0e26d41b0f2817910a7f58c79933115b623c4537
basupatil1213 <patil.ba@northeastern.edu>
2024-07-24 15:28:58 -0400

    SEO updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import { Home as LadingPage } from "@/components/home";
+import Head from "next/head";
 
 export default function Home() {
   return (
-     <LadingPage/>
+    <>
+      <Head>
+        <title>Basavaraj Patil - Full Stack Web Developer</title>
+        <meta name="description" content="Basavaraj Patil is a skilled web developer specializing in modern, responsive, and user-friendly websites and applications. Proficient in Python, Java, JavaScript, and various front-end and back-end technologies, with experience in cloud platforms and DevOps tools." />
+        <link rel="canonical" href="https://www.basavarajpatil.me/" />
+      </Head>
+      <main>
+        <LadingPage />
+      </main>
+    </>
   );
 }
